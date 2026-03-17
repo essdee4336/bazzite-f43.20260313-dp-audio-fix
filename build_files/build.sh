56e6b62b94bda6e00f670ca539c8b019e9ce2d97
essdee4336 <dustbustr@gmail.com>
2026-03-17 12:48:32 -0700

    How about this?

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+
 # Install packages for patched kernel w/ DisplayPort audio fix
 rpm -Uvh --force $(find /ctx/local-rpms -name "*.rpm")
 
